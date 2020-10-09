51b77f6e3ffcdf15e3b436c2287511eda37ad9e6
Erica Noronha <ericanoronhab@gmail.com>
2020-10-09 21:53:36 +0000

    trabalho final

--- trabalho.cpp
+++ trabalho.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <stdio.h>
@@ -6,263 +7,313 @@
 
 using namespace std;
 typedef struct No {
-  int codigo;
-  string nome;
-  int idade;
-  string CPF;
-  float divida;
-  int data_pagamento;
-  struct No* esq;
-  struct No* dir;
-  int altura;
-  void exibir_no() {
-    cout <<"Codigo: "<< this->codigo <<endl;
-    cout <<"Nome: "<< this->nome <<endl;
-    cout <<"Idade: "<< this->idade <<endl;
-    cout <<"CPF: "<< this->CPF <<endl;
-    cout <<"Dívida: "<< this->divida <<endl;
-    cout <<"Data de pagamento: "<< this->data_pagamento <<endl;
-    //cout <<"ALTURA NA ARVORE: "<< this->altura<<endl;
-    cout<<endl;
-  }
+int codigo;
+string nome;
+int idade;
+string CPF;
+float divida;
+string data_pagamento;
+struct No* esq;
+struct No* dir;
+int altura;
+
+void exibir_no() {
+cout <<"Codigo: "<< this->codigo <<endl;
+cout <<"Nome: "<< this->nome <<endl;
+cout <<"Idade: "<< this->idade <<endl;
+cout <<"CPF: "<< this->CPF <<endl;
+cout <<"Dívida: "<< this->divida <<endl;
+//cout <<"Data de pagamento: "<< this->data_pagamento <<endl;
+
+cout <<"Data de pagamento: "<< this->data_pagamento.substr(0,2) << "/" << this->data_pagamento.substr(2,2) << "/" << this->data_pagamento.substr(4,4) 
+<<endl;
+
+//cout <<"ALTURA NA ARVORE: "<< this->altura<<endl;
+cout<<endl;
+}
 } No;
 
 
 class Arvore {
-  No* raiz;
-  No* maior_divida;
-  float acumulador_de_dividas;
-  int numero_de_dividas;
+No* raiz;
+No* maior_divida;
+float acumulador_de_dividas;
+int numero_de_dividas;
 
-  No* inserir(int codigo, string nome, int idade, string CPF, float divida, int data_pagamento, No* A) {
-    if(A == NULL) {
-      A = new No;
-      A->codigo = codigo;
-      A->nome=nome;
-      A->idade=idade;
-      A->CPF=CPF;
-      A->divida=divida;
-      A->data_pagamento=data_pagamento;
-      A->altura = 0;
-      A->esq = A->dir = NULL;
 
-      if((A->divida)>this->maior_divida->divida)
-        this->maior_divida = A;
+No* inserir(int codigo, string nome, int idade, string CPF, float divida, string data_pagamento, No* A) {
+if(A == NULL) {
+A = new No;
+A->codigo = codigo;
+A->nome=nome;
+A->idade=idade;
+A->CPF=CPF;
+A->divida=divida;
+A->data_pagamento=data_pagamento;
+A->altura = 0;
+A->esq = A->dir = NULL;
 
-      acumulador_de_dividas+=A->divida;
-      this->numero_de_dividas++;
-    } else if(codigo < A->codigo) {
-      A->esq = inserir(codigo, nome, idade, CPF, divida, data_pagamento, A->esq);
-      if(altura(A->esq) - altura(A->dir) == 2) {
-        if(codigo < A->esq->codigo)
-          A = rotacao_simples_a_direita(A);
-        else
-          A = rotacao_dupla_a_direita(A);
-      }
-    } else if(codigo > A->codigo) {
-      A->dir = inserir(codigo, nome, idade, CPF, divida, data_pagamento, A->dir);
-      if(altura(A->dir) - altura(A->esq) == 2) {
-        if(codigo > A->dir->codigo)
-          A = rotacao_simples_a_esquerda(A);
-        else
-          A = rotacao_dupla_a_esquerda(A);
-      }
-    }
-    A->altura = max(altura(A->esq), altura(A->dir))+1;
 
-    return A;
-  }
+if((A->divida)>this->maior_divida->divida)
+this->maior_divida = A;
 
-  No* rotacao_simples_a_direita(No* &A) {
-    No* B = A->esq;
-    A->esq = B->dir;
-    B->dir = A;
-    A->altura = max(altura(A->esq), altura(A->dir))+1;
-    B->altura = max(altura(B->esq), A->altura)+1;
-    return B;
-  }
+acumulador_de_dividas+=A->divida;
+this->numero_de_dividas++;
+} else if(codigo < A->codigo) {
+A->esq = inserir(codigo, nome, idade, CPF, divida, data_pagamento, A->esq);
+if(altura(A->esq) - altura(A->dir) == 2) {
+if(codigo < A->esq->codigo)
+A = rotacao_simples_a_direita(A);
+else
+A = rotacao_dupla_a_direita(A);
+}
+} else if(codigo > A->codigo) {
+A->dir = inserir(codigo, nome, idade, CPF, divida, data_pagamento, A->dir);
+if(altura(A->dir) - altura(A->esq) == 2) {
+if(codigo > A->dir->codigo)
+A = rotacao_simples_a_esquerda(A);
+else
+A = rotacao_dupla_a_esquerda(A);
+}
+}
+A->altura = max(altura(A->esq), altura(A->dir))+1;
 
-  No* rotacao_simples_a_esquerda(No* &A) {
-    No* B = A->dir;
-    A->dir = B->esq;
-    B->esq = A;
-    A->altura = max(altura(A->esq), altura(A->dir))+1;
-    B->altura = max(altura(A->dir), A->altura)+1 ;
-    return B;
-  }
+return A;
+}
 
-  No* rotacao_dupla_a_esquerda(No* &A) {
-    A->dir = rotacao_simples_a_direita(A->dir);
-    return rotacao_simples_a_esquerda(A);
-  }
+No* rotacao_simples_a_direita(No* &A) {
+No* B = A->esq;
+A->esq = B->dir;
+B->dir = A;
+A->altura = max(altura(A->esq), altura(A->dir))+1;
+B->altura = max(altura(B->esq), A->altura)+1;
+return B;
+}
 
-  No* rotacao_dupla_a_direita(No* &A) {
-    A->esq = rotacao_simples_a_esquerda(A->esq);
-    return rotacao_simples_a_direita(A);
-  }
+No* rotacao_simples_a_esquerda(No* &A) {
+No* B = A->dir;
+A->dir = B->esq;
+B->esq = A;
+A->altura = max(altura(A->esq), altura(A->dir))+1;
+B->altura = max(altura(A->dir), A->altura)+1 ;
+return B;
+}
 
-  No* para_encontrar_o_minimo_codigo(No* A) {
-    if(A!=NULL)
-      while(A->esq!=NULL)
-        A = A->esq;
-    return A;
-  }
+No* rotacao_dupla_a_esquerda(No* &A) {
+A->dir = rotacao_simples_a_direita(A->dir);
+return rotacao_simples_a_esquerda(A);
+}
 
-  No* para_encontrar_o_maximo_codigo(No* A) {
-    if(A == NULL)
-      return NULL;
-    else if(A->dir == NULL)
-      return A;
-    else
-      return para_encontrar_o_maximo_codigo(A->dir);
-  }
+No* rotacao_dupla_a_direita(No* &A) {
+A->esq = rotacao_simples_a_esquerda(A->esq);
+return rotacao_simples_a_direita(A);
+}
 
-  int altura(No* A) {
-    return (A == NULL ? -1 : A->altura);
-  }
+No* para_encontrar_o_minimo_codigo(No* A) {
+if(A!=NULL)
+while(A->esq!=NULL)
+A = A->esq;
+return A;
+}
 
-  int retorna_fator_de_balanceameno(No* A) {
-    if(A == NULL)
-      return 0;
-    else
-      return altura(A->dir) - altura(A->esq);
-  }
+No* para_encontrar_o_maximo_codigo(No* A) {
 
-  void inorder(No* A) {
-    if(A == NULL)
-      return;
-    inorder(A->esq);
-    A->exibir_no();
-    cout<<endl;
-    inorder(A->dir);
-  }
+if(A == NULL) 
+return NULL;
+else if(A->dir == NULL) 
+return A;
+else
+return para_encontrar_o_maximo_codigo(A->dir);
+}
 
-  No* busca_recursiva(int codigo,No* raiz_Sub_Arvore) {
+int altura(No* A) {
+return (A == NULL ? -1 : A->altura);
+}
 
-    if(codigo==raiz_Sub_Arvore->codigo)
-      return raiz_Sub_Arvore;
+int retorna_fator_de_balanceameno(No* A) {
+if(A == NULL)
+return 0;
+else
+return altura(A->dir) - altura(A->esq);
+}
 
-    else if(codigo>raiz_Sub_Arvore->codigo)
-      return busca_recursiva(codigo,raiz_Sub_Arvore->dir);
-    else return busca_recursiva(codigo,raiz_Sub_Arvore->esq);
+void inorder(No* A) {
+if(A == NULL)
+return;
+inorder(A->esq);
+A->exibir_no();
+cout<<endl;
+inorder(A->dir);
+}
 
-  }
 
-  No* remover_no( int codigo,No* A) {
-    if (A == NULL)
-      return A;
+No* busca_recursiva(int codigo,No* raiz_Sub_Arvore) {
 
-    if ( codigo < A->codigo )
-      A->esq = remover_no(codigo,A->esq);
+if(codigo==raiz_Sub_Arvore->codigo)
+return raiz_Sub_Arvore;
 
-    else if(codigo > A->codigo )
-      A->dir = remover_no(codigo,A->dir);
+else if(codigo>raiz_Sub_Arvore->codigo)
+return busca_recursiva(codigo,raiz_Sub_Arvore->dir);
+else return busca_recursiva(codigo,raiz_Sub_Arvore->esq);
 
-    else {
-      if( (A->esq == NULL) || (A->dir == NULL) ) {
-        No *temp = A->esq ? A->esq : A->dir;
+}
 
-        if (temp == NULL) {
-          temp = A;
-          A = NULL;
-        } else
-          *A = *temp;
-        delete(temp);
-      } else {
+No* remover_no( int codigo,No* A) {
+if (A == NULL)
+return A;
 
-        No* temp = para_encontrar_o_minimo_codigo(A->dir);
+if ( codigo < A->codigo )
+A->esq = remover_no(codigo,A->esq);
 
-        A->codigo = temp->codigo;
+else if(codigo > A->codigo )
+A->dir = remover_no(codigo,A->dir);
 
-        A->dir = remover_no( temp->codigo,A->dir);
-      }
-    }
+else {
+if( (A->esq == NULL) || (A->dir == NULL) ) {
+No *temp = A->esq ? A->esq : A->dir;
 
-    if (A == NULL)
-      return A;
+if (temp == NULL) {
+temp = A;
+A = NULL;
+} else
+*A = *temp;
+delete(temp);
+} else {
 
-    A->altura = 1 + max(altura(A->esq),altura(A->dir));
+No* temp = para_encontrar_o_minimo_codigo(A->dir);
 
-    int balance = retorna_fator_de_balanceameno(A);
+A->codigo = temp->codigo;
 
-    if (balance > 1 && retorna_fator_de_balanceameno(A->esq) >= 0)
-      return rotacao_simples_a_esquerda(A);
+A->dir = remover_no( temp->codigo,A->dir);
+}
+}
 
-    if (balance > 1 && retorna_fator_de_balanceameno(A->esq) < 0) {
-      return rotacao_dupla_a_esquerda(A);
-    }
+if (A == NULL)
+return A;
 
-    if (balance < -1 && retorna_fator_de_balanceameno(A->dir) <= 0)
-      return rotacao_simples_a_direita(A);
+A->altura = 1 + max(altura(A->esq),altura(A->dir));
 
-    if (balance < -1 && retorna_fator_de_balanceameno(A->dir) > 0) {
-      return rotacao_dupla_a_direita(A);
-    }
-    return A;
-  }
+int balance = retorna_fator_de_balanceameno(A);
+
+if (balance > 1 && retorna_fator_de_balanceameno(A->esq) >= 0)
+return rotacao_simples_a_esquerda(A);
+
+if (balance > 1 && retorna_fator_de_balanceameno(A->esq) < 0) {
+return rotacao_dupla_a_esquerda(A);
+}
+
+if (balance < -1 && retorna_fator_de_balanceameno(A->dir) <= 0)
+return rotacao_simples_a_direita(A);
+
+if (balance < -1 && retorna_fator_de_balanceameno(A->dir) > 0) {
+return rotacao_dupla_a_direita(A);
+}
+return A;
+}
 
 public:
 
-  void remover(int codigo) {
-    raiz=remover_no(codigo,raiz);
-  }
+void remover(int codigo) {
+raiz=remover_no(codigo,raiz);
+}
 
-  void exibe_intervalo(No* A, float divida1, float divida2) {
-    if(A == NULL)
-      return;
-    exibe_intervalo(A->esq,divida1,divida2);
-    if( divida1<=A->divida &&  A->divida<=divida2 ) {
-      A->exibir_no();
-      cout<<endl;
-    }
-    exibe_intervalo(A->dir,divida1,divida2);
-  }
+// FS
+void exibe_em_atraso(No* A) {
+if(A == NULL)
+return;
 
-  Arvore() {
-    raiz = NULL;
-    maior_divida=new No;
-    maior_divida->divida=DBL_MIN;
-    this->acumulador_de_dividas=0.0;
-    this->numero_de_dividas=0;
-  }
+//stoi(A->data_pagamento.substr(4,4)) ANO
+//stoi(A->data_pagamento.substr(2,2)) MES 
+//stoi(A->data_pagamento.substr(0,2)) DIA
 
-  float get_valor_medio_das_dividas() {
-    return this->acumulador_de_dividas/(float)this->numero_de_dividas;
-  }
+exibe_em_atraso(A->esq);
+if ((stoi(A->data_pagamento.substr(4,4)) == 2020
+&& stoi(A->data_pagamento.substr(2,2)) >= 9 ) || 
+(stoi(A->data_pagamento.substr(0,2)) >= 2020
+)) {
+A->exibir_no();
+cout<<endl;
+}
+exibe_em_atraso(A->dir);
 
-  No* get_maior_divida() {
-    return this->maior_divida;
-  }
+}
 
-  int get_numero_de_devedores() {
-    return this->numero_de_dividas;
-  }
+// Fim FS
 
-  void inserir_interface(int codigo, string nome, int idade, string CPF, float divida, int data_pagamento) {
-    raiz = inserir(codigo, nome, idade, CPF, divida, data_pagamento, raiz);
-  }
+void exibe_intervalo(No* A, float divida1, float divida2) {
+if(A == NULL)
+return;
+exibe_intervalo(A->esq,divida1,divida2);
+if( divida1<=A->divida && A->divida<=divida2 ) {
+A->exibir_no();
+cout<<endl;
+}
+exibe_intervalo(A->dir,divida1,divida2);
+}
 
-  void exibir() {
-    inorder(raiz);
-    cout << endl;
-  }
+Arvore() {
+raiz = NULL;
+maior_divida=new No;
+maior_divida->divida=DBL_MIN;
+this->acumulador_de_dividas=0.0;
+this->numero_de_dividas=0;
+}
 
-  void exibir_no_intervalo(float divida1, float divida2) {
-    if(divida1<=0||divida2<=0) {
-      cout<<endl<<"dividas invalidas";
-      return;
-    }
-    if(divida1>divida2)
-      swap(divida1,divida2);
+float get_valor_medio_das_dividas() {
+return this->acumulador_de_dividas/(float)this->numero_de_dividas;
+}
 
-    exibe_intervalo(raiz,divida1,divida2);
-    cout << endl;
-  }
+No* get_maior_divida() {
+return this->maior_divida;
+}
 
-  No* busca(int codigo) {
-    No* i = this->raiz;
-    i=busca_recursiva(codigo,i);
-    return i;
-  }
+int get_numero_de_devedores() {
+return this->numero_de_dividas;
+}
+
+void inserir_interface(int codigo, string nome, int idade, string CPF, float divida, string data_pagamento) {
+raiz = inserir(codigo, nome, idade, CPF, divida, data_pagamento, raiz);
+}
+
+void exibir() {
+inorder(raiz);
+cout << endl;
+}
+
+// FS
+void exibir_a_maior_divida() {
+cout <<"Codigo: "<< this->maior_divida->codigo <<endl;
+cout <<"Nome: "<< this->maior_divida->nome <<endl;
+cout <<"CPF: "<< this->maior_divida->CPF <<endl;
+cout <<"Dívida: "<< this->maior_divida->divida <<endl;
+
+cout << endl;
+}
+
+void exibir_dividas_em_atraso() {
+exibe_em_atraso(raiz);
+cout << endl;
+}
+
+
+// Fim FS
+
+void exibir_no_intervalo(float divida1, float divida2) {
+if(divida1<=0||divida2<=0) {
+cout<<endl<<"dividas invalidas";
+return;
+}
+if(divida1>divida2)
+swap(divida1,divida2);
+
+exibe_intervalo(raiz,divida1,divida2);
+cout << endl;
+}
+
+No* busca(int codigo) {
+No* i = this->raiz;
+i=busca_recursiva(codigo,i);
+return i;
+}
 
 };
